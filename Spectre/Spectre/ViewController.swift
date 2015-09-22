582b7feefe3e1e80496389235850ee37f16a4f6b
pasquali <pasquali@cmtoolss-mbp.auth.hpicorp.net>
2015-09-22 09:35:44 -0300

    First commit

---
+++ Spectre/Spectre/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spectre
+//
+//  Created by pasquali on 9/22/15.
+//  Copyright © 2015 MarinaPasquali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
