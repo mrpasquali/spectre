a4bbf8c30a717df21553fe9d62a982155c316e01
Marina <marinapasquali@pedros-mbp.auth.hpicorp.net>
2015-10-21 15:45:59 -0200

    Migrated from old repo to new repo.

--- Spectre/Spectre/ViewController.swift
+++ Spectre/Spectre/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import Alamofire
 
 class SearchViewController: UIViewController {
 
     @IBOutlet weak var firstTitle: UILabel!
-    @IBOutlet weak var enterAction: UILabel!
     @IBOutlet weak var okButton: UIButton!
-   
-    override func viewDidLoad        super.viewDidLoad()
-        
-        enterAction.layer.cornerRadius = 100
-        enterAction.layer.borderWidth = 800
-        enterAction.layer.borderColor = UIColor.whiteColor().CGColor
+    @IBAction func enterAction(sender: AnyObject) {
     }
     
- }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Alamofire.request(
+            .GET, // GET, POST, etc...
+            "https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/G3/2161/dep/2015/10/20", // URL
+            parameters: [
+                "appId": "7351e7ee",
+                "appKey": "780f8112018790fe400f3cec7782fa26",
+                "utc": "false"])
+            .responseJSON { response in
+                debugPrint(response)
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+                
+        }
+    }
+}
 
